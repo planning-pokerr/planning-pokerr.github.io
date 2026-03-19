daaa7c23a15b282c4261ab7ac773cdfa1acd9144
Shivang Sharma <70644519+shivang-sharma@users.noreply.github.com>
2026-03-19 18:06:33 +0530

    Change base URL for Vite configuration

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/planningpoker.github.io/',
   optimizeDeps: {
     include: ['yjs', 'trystero/nostr'],
   },
